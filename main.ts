834a067a20551b86b25ac1d1c4d0191476932935
jnano-07 <71751213+jnano-07@users.noreply.github.com>
2020-11-18 15:56:00 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,8 @@ let x0 = 2
 let y0 = 2
 led.plot(x0, y0)
 for (let i = 0; i <= 2; i++) {
-    if (true) {
-    	
+    if (0 == 0 || 0 == 0) {
+        led.plot(x0, y0 + i)
+        basic.pause(100)
     }
 }
-basic.forever(function () {
-	
-})
